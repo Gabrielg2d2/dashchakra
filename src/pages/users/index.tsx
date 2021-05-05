82a52edf99e19d03f7c8f9e9f99629bee1e917cc
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-05-05 19:48:22 -0300

    feat: created hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -20,35 +20,10 @@ import { Pagination } from 'components/Pagination'
 import Sidebar from 'components/Sidebar'
 import Link from 'next/link'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
-import { useQuery } from 'react-query'
-
-type User = {
-  id: string
-  name: string
-  email: string
-  createdAt: string
-}
+import { useUsers } from 'services/hooks/userUsers'
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
-
-    const users: User[] = data.users.map((user: User) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
